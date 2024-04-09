9d36f7573267574b9b9f3f43ceacb460e3d5708d
evanchen12 <71461932+evanchen12@users.noreply.github.com>
2024-04-09 13:36:10 -0400

    fixed choiceQ model

--- Quiz/ChoiceQs/model.js
+++ Quiz/ChoiceQs/model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import ChoiceQsSchema from "./schema.js";
 
-const model = mongoose.model("OptionsModel", ChoiceQsSchema);
+const model = mongoose.model("ChoiceQsModel", ChoiceQsSchema);
 
 export default model;
