92791a70f7561417ab2a8d4027f35df503f4a5c1
chen-luke <72319988+chen-luke@users.noreply.github.com>
2024-04-14 17:22:58 -0400

    created working crud operations for quizzes

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from 'mongoose';
 import QuizzesRoutes from "./Quiz/Quizzes/routes.js";
-import ChoiceQsRoutes from "./Quiz/ChoiceQs/routes.js";
-import OptionsRoutes from "./Quiz/Options/routes.js";
+// import ChoiceQsRoutes from "./Quiz/ChoiceQs/routes.js";
+// import OptionsRoutes from "./Quiz/Options/routes.js";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/quiz';
 const app = express();
 mongoose.connect(CONNECTION_STRING);
 
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 QuizzesRoutes(app);
-ChoiceQsRoutes(app);
-OptionsRoutes(app);
+// ChoiceQsRoutes(app);
+// OptionsRoutes(app);
 
 app.listen(process.env.PORT || 4000);
