e8b1582ed8c035106c4d9d4a70f76d5a83421495
evanchen12 <71461932+evanchen12@users.noreply.github.com>
2024-04-17 00:50:39 -0400

    Finished

--- App.js
+++ App.js
@@ -2,19 +2,42 @@ import express from 'express';
 import cors from "cors";
 import "dotenv/config";
 import mongoose from 'mongoose';
+import session from 'express-session';
 import QuizzesRoutes from "./Quiz/Quizzes/routes.js";
 import ChoiceQsRoutes from "./Quiz/ChoiceQs/routes.js";
 import OptionsRoutes from "./Quiz/Options/routes.js";
+import UserRoutes from './Users/routes.js';
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
 const app = express();
+const sessionOptions = {
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+};
+
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+  };
+}
+
 mongoose.connect(CONNECTION_STRING);
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.FRONTEND_URL,
+  })
+); 
+app.use(session(sessionOptions));
 app.use(express.json());
 
 QuizzesRoutes(app);
 ChoiceQsRoutes(app);
 OptionsRoutes(app);
+UserRoutes(app);
 
 app.listen(process.env.PORT || 4000);
